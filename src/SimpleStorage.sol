aa2feb22d3eab642202e1538c6dc75741f0dabac
Deep912 <deepchhegaliya@gmail.com>
2025-01-18 16:41:39 +0530

    setting up

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,25 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract simple{
-// struct is used to define custom type
-uint256 myFavoriteNumber;
-struct Person{
-    string name;
-    uint256 number;
-    
-}
+contract simple {
+    // struct is used to define custom type
+    uint256 myFavoriteNumber;
+    struct Person {
+        string name;
+        uint256 number;
+    }
 
-//we are making array of Person(struct) aka dynamic array
-Person[] public listOfPeople;
+    //we are making array of Person(struct) aka dynamic array
+    Person[] public listOfPeople;
 
-//mapping a value to another value for ex string => uint256 = deep => 10 so string "deep" is now mapped with uint256 10 number
-mapping (string => uint256) public nameToNumber;
-//static array
-// Person[3] public listOfPeople;
+    //mapping a value to another value for ex string => uint256 = deep => 10 so string "deep" is now mapped with uint256 10 number
+    mapping(string => uint256) public nameToNumber;
 
-//added virtual keyword so function can be override in another contract when using keyword override,
-// so virtual tells that this function can be override
+    //static array
+    // Person[3] public listOfPeople;
+
+    //added virtual keyword so function can be override in another contract when using keyword override,
+    // so virtual tells that this function can be override
     function store(uint256 _favoriteNumber) public virtual {
         myFavoriteNumber = _favoriteNumber;
     }
@@ -28,11 +28,10 @@ mapping (string => uint256) public nameToNumber;
         return myFavoriteNumber;
     }
 
-function addPeople(string memory _name,uint256 _number) public {
-    listOfPeople.push(Person( _name ,_number)); //pushing new people in Person
-    nameToNumber[_name] = _number; // we added function that will map name with number so deep to 10
-}
+    function addPeople(string memory _name, uint256 _number) public {
+        listOfPeople.push(Person(_name, _number)); //pushing new people in Person
+        nameToNumber[_name] = _number; // we added function that will map name with number so deep to 10
+    }
 
-// Person public myFriend = Person({ number : 7 , name: "pet"});
-
+    // Person public myFriend = Person({ number : 7 , name: "pet"});
 }
